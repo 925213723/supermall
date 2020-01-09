7e5c0be95b9ae2c180ead0ece64a2e3afab27c60
wangyuanshun <925213723@qq.com>
2020-01-09 19:24:30 +0800

    '轮播图未完'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/nvabar/NvaBar'
+  import { getHomeMultidata } from 'network/home'
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      NavBar
+    },
+    created() {
+      console.log('qwertui')
+      getHomeMultidata().then(res => {
+        console.log(res)
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    font-size: 20px;
+  }
 </style>
