a8341b6a35bc05367e5548534aac213a7bdf1a88
wangyuanshun <925213723@qq.com>
2020-01-10 19:04:32 +0800

    'home页面'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,40 @@
 <template>
   <div id="home">
     <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+    <home-swiper :banner = banner></home-swiper>
+    <recommend :recommend = recommend></recommend>
+    <feature-view></feature-view>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/nvabar/NvaBar'
+
+  import HomeSwiper from './childComps/HomeSwiper'
+  import Recommend from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatrueView'
+
   import { getHomeMultidata } from 'network/home'
   export default {
     name: 'Home',
+    data() {
+      return {
+        result: null,
+        banner: [],
+        recommend: []
+     }
+    },
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper,
+      Recommend,
+      FeatureView
     },
     created() {
-      console.log('qwertui')
       getHomeMultidata().then(res => {
-        console.log(res)
+        this.result = res
+        this.banner = res.data.banner.list
+        this.recommend = res.data.recommend.list
       })
     }
   }
@@ -26,5 +45,11 @@
     background-color: var(--color-tint);
     color: #fff;
     font-size: 20px;
+
+    position: flexd;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    z-index: 99;
   }
 </style>
